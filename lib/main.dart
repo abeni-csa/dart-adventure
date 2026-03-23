d77e9bdd6560470aeccfd4a6edc35a0c12d7a1e3
abenezer@csa <abenezerhaymanot@gmail.com>
2026-03-23 11:09:01 +0300

    remove appBar.. Grid View use full for drowing Wedigs every wehere in x and y axix

--- lib/main.dart
+++ lib/main.dart
@@ -12,68 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Temar Lije"),
-          backgroundColor: Colors.amber,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-          centerTitle: true,
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(12),
-          scrollDirection: Axis.vertical,
-
-          children: [
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.green),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.yellow),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.red),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.green),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.yellow),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.red),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.green),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.yellow),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.red),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.green),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.yellow),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.red),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.green),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.yellow),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.red),
-            ),
-            Expanded(
-              child: Container(height: 150, width: 150, color: Colors.black),
-            ),
-          ],
+        body: GridView.builder(
+          itemCount: 64,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+          ),
+          itemBuilder: (contex, index) =>
+              Container(color: Colors.redAccent, margin: EdgeInsets.all(12)),
         ),
       ),
     );
