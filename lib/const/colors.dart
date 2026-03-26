dce3cce0555ae84ffe383e0a5e9e30f67dab8f83
abenezer@csa <abenezerhaymanot@gmail.com>
2026-03-26 13:21:37 +0300

    pkg updated and stat Pircipal Dashbord

---
+++ lib/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromARGB(15, 241, 88, 255);
+const secondryColor = Color.fromARGB(255, 39, 51, 119);
+const bgColor = Color(0xFF212332);
+
+const defaultPadding = 16.0;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:fist_flutter/pages/1stpage.dart';
+import 'package:fist_flutter/const/colors.dart';
+import 'package:fist_flutter/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FirstPage(), debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Temar Lije Principal Dashbord",
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme.apply(bodyColor: Colors.white),
+        ),
+        canvasColor: secondryColor,
+      ),
+      home: MainScreen(),
+    );
   }
 }
