c8166de49c81845d29c05fef348cebdafdf96102
abenezer@csa <abenezerhaymanot@gmail.com>
2026-03-28 13:33:17 +0300

    components for repted wedgits

---
+++ lib/components/search_box.dart
@@ -0,0 +1,34 @@
+import 'package:fist_flutter/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SearchField extends StatelessWidget {
+  const SearchField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search",
+        fillColor: secondryColor,
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+        ),
+        suffixIcon: InkWell(
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.all(defaultPadding * 0.75),
+            margin: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+            child: SvgPicture.asset('assets/icons/search.svg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
