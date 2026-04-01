0504a3e0b91a3662239aa9bfa76afbfaa8bfe2b6
abenezer@csa <abenezerhaymanot@gmail.com>
2026-04-01 10:33:54 +0300

    Mastring Flutter 1st chapter

---
+++ lib/ui/screens/home/horiz_movies.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class HorizontalMovies extends StatelessWidget {
+  final List<String> movies;
+
+  const HorizontalMovies({Key? key, required this.movies}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 142,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: SizedBox(
+              width: 100,
+              height: 142,
+              child: CachedNetworkImage(
+                imageUrl: movies[index],
+                alignment: Alignment.topCenter,
+                fit: BoxFit.fitHeight,
+                height: 100,
+                width: 142,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
