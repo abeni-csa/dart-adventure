3e0994e89f9e36b85b89f29816e7f7a6604f99c3
abenezer@csa <abenezerhaymanot@gmail.com>
2026-04-04 10:57:03 +0300

    reusable widgets

---
+++ lib/ui/widgets/vert_movie_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fist_flutter/utils/utils.dart';
+
+import 'movie_row.dart';
+
+typedef OnMovieTap = void Function(int movieId);
+
+class VerticalMovieList extends ConsumerWidget {
+  final List<String> movies;
+  final OnMovieTap onMovieTap;
+
+  const VerticalMovieList({
+    super.key,
+    required this.movies,
+    required this.onMovieTap,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (movies.isEmpty) {
+      return const SizedBox.shrink();
+    }
+    return Column(
+      children: [
+        movies.length > 1
+            ? MovieRow(movie: movies[0])
+            : const SizedBox.shrink(),
+        addVerticalSpace(10),
+        movies.length > 2
+            ? MovieRow(movie: movies[1])
+            : const SizedBox.shrink(),
+      ],
+    );
+  }
+}
